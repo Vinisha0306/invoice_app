5ff3774e95bfb9213c0a07864219decee1715115
Aneri09 <aneridobariya9@gmail.com>
2024-03-15 12:19:52 +0530

    customer

--- lib/pages/build_options/customer_details/customer_details.dart
+++ lib/pages/build_options/customer_details/customer_details.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class InvoicePage extends StatefulWidget {
@@ -12,13 +13,47 @@ class _InvoicePageState extends State<InvoicePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(
+        title: const Text(
           'Add Invoice',
           style: TextStyle(
             fontWeight: FontWeight.bold,
           ),
         ),
         backgroundColor: const Color.fromARGB(255, 183, 153, 255),
+      ),
+      backgroundColor: const Color.fromARGB(255, 183, 153, 255),
+      body: Padding(
+        padding: EdgeInsets.all(16),
+        child: Container(
+          padding: EdgeInsets.all(10),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            color: Colors.white,
+          ),
+          alignment: Alignment.topLeft,
+          child: Column(
+            children: <Widget>[
+              SizedBox(
+                width: 200,
+                child: OutlinedButton(
+                  onPressed: () {
+                    showDialog(
+                      context: context,
+                      builder: (context) => AlertDialog(
+                        title: Text(
+                          'Customer Details',
+                        ),
+                      ),
+                    );
+                  },
+                  child: Text(
+                    'Customer Details',
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
